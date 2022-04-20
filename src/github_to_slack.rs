648924e30f63f656e2c7fa0fc154d82c5d8f7f3f
alabulei1 <vivian.xiage@gmail.com>
2022-04-20 14:24:59 +0800

    filter by @alabulei1

--- src/github_to_slack.rs
+++ src/github_to_slack.rs
@@ -5,6 +5,9 @@ use serde_json::{Result, Value};
 #[wasmedge_bindgen]
 pub fn run(s: String) -> String {
 	let result: Result<Value> = serde_json::from_str(s.as_str());
+	let mut body: String = String::new();
+	let mut event_type: &str = "";
+	let mut html_url: &str = "";
 	if let Ok(pl) = result {
 		match pl.get("action") {
 			Some(action) => {
@@ -12,21 +15,34 @@ pub fn run(s: String) -> String {
 					match action {
 						"opened" => {
 							if let Some(issue) = pl.get("issue") {
-								return format!("issue created\n{}", issue.get("body").unwrap().as_str().unwrap().replace("\\r\\n", "\n"));
+								event_type = "issue created";
+								body = issue.get("body").unwrap().as_str().unwrap().to_string();
+								html_url = issue.get("html_url").unwrap().as_str().unwrap();
 							}
 						}
 						"edited" => {
 							if let Some(comment) = pl.get("comment") {
-								return format!("issue comment edited\n{}", comment.get("body").unwrap().as_str().unwrap().replace("\\r\\n", "\n"));
+								event_type = "issue comment edited";
+								body = comment.get("body").unwrap().as_str().unwrap().to_string();
+								html_url = comment.get("html_url").unwrap().as_str().unwrap();
 							} else if let Some(issue) = pl.get("issue") {
-								return format!("issue edited\n{}", issue.get("body").unwrap().as_str().unwrap().replace("\\r\\n", "\n"));
+								event_type = "issue edited";
+								body = issue.get("body").unwrap().as_str().unwrap().to_string();
+								html_url = issue.get("html_url").unwrap().as_str().unwrap();
 							}
 						}
 						"created" => {
-							if let Some(issue) = pl.get("starred_at") {
-								return format!("start created");
-							} else if let Some(comment) = pl.get("comment") {
-								return format!("issue comment created\n{}", comment.get("body").unwrap().as_str().unwrap().replace("\\r\\n", "\n"));
+							if let Some(comment) = pl.get("comment") {
+								event_type = "issue comment created";
+								html_url = comment.get("html_url").unwrap().as_str().unwrap();
+								body = comment.get("body").unwrap().as_str().unwrap().to_string();
+							}
+						}
+						"assigned" => {
+							if let Some(assignee) = pl.get("assignee") {
+								event_type = "issue assigned";
+								html_url = pl.get("issue").unwrap().get("html_url").unwrap().as_str().unwrap();
+								body = format!("Issue assigned to @{}.", assignee.get("login").unwrap().as_str().unwrap());
 							}
 						}
 						_ => {
@@ -39,7 +55,13 @@ pub fn run(s: String) -> String {
 				return "".to_string();
 			}
 		}
+		if event_type != "" {
+			if body.contains("@alabulei1") {
+				return format!("{}\n{}\n{}", event_type, body.replace("\\r\\n", "\n"), html_url);
+			}
+		}
 	}
 
-	return s;
+
+	return "".to_string();
 }
