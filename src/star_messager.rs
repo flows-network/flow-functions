daa0fbf533ccca23e20b687cec34edcf4e18093b
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-07-13 12:26:06 +0800

    Send error message  as String in the Result

--- src/star_messager.rs
+++ src/star_messager.rs
@@ -1,33 +1,33 @@
 #[allow(unused_imports)]
 use wasmedge_bindgen::*;
 use wasmedge_bindgen_macro::*;
-use serde_json::{Result, Value, json};
+use serde_json::Value;
 
 #[wasmedge_bindgen]
-pub fn run(s: String) -> String {
+pub fn run(s: String) -> Result<String, String> {
     let res: Value = match serde_json::from_str(s.as_str()) {
         Ok(s) => s,
-        Err(e) => return "GitHub webhook payloads parsing failed.".to_string(),
+        Err(e) => return Err(format!("GitHub webhook payloads parsing failed: {}.", e.to_string())),
     };
 
     let is_star = match res["action"].as_str() {
         Some(action) => action == "created",
-        None => return "Parse action failed.".to_string(),
+        None => return Err("Parse action failed.".to_string()),
     };
 
     let name = match res["repository"]["full_name"].as_str() {
         Some(n) => n,
-        None => return "Parse repository name failed.".to_string(),
+        None => return Err("Parse repository name failed.".to_string()),
     };
 
     let stargazers_count = match res["repository"]["stargazers_count"].as_u64() {
         Some(c) => c,
-        None => return "Parse stargazers_count failed.".to_string(),
+        None => return Err("Parse stargazers_count failed.".to_string()),
     };
 
     if stargazers_count % 10 == 0 && is_star {
-        return format!("Congratulations on your repository {} with {} stars.", name, stargazers_count);
+        return Ok(format!("Congratulations on your repository {} with {} stars.", name, stargazers_count));
     }
 
-    "".to_string()
+    Ok("".to_string())
 }
