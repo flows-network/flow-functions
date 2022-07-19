c64c5f2b3ee0324aa833fe7ffa0e6b2d9c43d6b9
DarumaDocker <DarumaDocker@gmail.com>
2022-07-19 12:39:31 +0800

    calculator return picture

--- slack/slack/calculator/src/calculator.rs
+++ slack/slack/calculator/src/calculator.rs
@@ -12,23 +12,49 @@ pub fn _run(exp: String) -> String {
     return e;
 }
 
-// #[wasmedge_bindgen]
-// pub fn run(exp: String) -> (String, String, String, Vec<u8>) {
-// 	let r = meval::eval_str(&exp).unwrap();
-// 	let e = format!("{} = {}", exp, r);
+#[wasmedge_bindgen]
+pub fn run(exp: String) -> Result<(String, String, String, Vec<u8>), String> {
+    let r = meval::eval_str(&exp).unwrap();
+    let e = format!("{} = {}", exp, r);
 
-// 	let renderer = TextRenderer::try_new_with_ttf_font_data(include_bytes!("/home/darumadocker/Downloads/Monoton-Regular.ttf"))
-//     	.expect("font is definitely loadable");
+    let renderer =
+        TextRenderer::try_new_with_ttf_font_data(include_bytes!("./Monoton-Regular.ttf"))
+            .expect("font is definitely loadable");
 
-// 	let text_png = renderer.render_text_to_png_data(
-// 		e,
-// 		200,
-// 		0x439EC2).unwrap();
-// 	return (String::new(), "math.png".to_string(), "image/png".to_string(), text_png.data);
-// }
+    let text_png = renderer.render_text_to_png_data(e, 200, 0x439EC2).unwrap();
+    Ok((
+        String::new(),
+        "math.png".to_string(),
+        "image/png".to_string(),
+        text_png.data,
+    ))
+}
 
-// #[wasmedge_bindgen]
-// pub fn run_file_1(exp: String, pic: Vec<u8>) -> (String, String, String, Vec<u8>) {
-// 	let r = meval::eval_str(&exp).unwrap();
-// 	return (format!("{} = {}", exp, r), "1.png".to_string(), "image/png".to_string(), pic);
-// }
+#[wasmedge_bindgen]
+pub fn run_file_1(exp: String, pic: Vec<u8>) -> (String, String, String, Vec<u8>) {
+    let r = meval::eval_str(&exp).unwrap();
+    return (
+        format!("{} = {}", exp, r),
+        "1.png".to_string(),
+        "image/png".to_string(),
+        pic,
+    );
+}
+
+#[wasmedge_bindgen]
+pub fn run_file_2(
+    exp: String,
+    pic: Vec<u8>,
+    pic2: Vec<u8>,
+) -> (String, String, String, Vec<u8>, String, String, Vec<u8>) {
+    let r = meval::eval_str(&exp).unwrap();
+    return (
+        format!("{} = {}", exp, r),
+        "1.png".to_string(),
+        "image/png".to_string(),
+        pic2,
+        "2.png".to_string(),
+        "image/png".to_string(),
+        pic,
+    );
+}
