6b02031fca6345edc7ed946380dfbf0e217dddc7
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-07-29 19:07:07 +0800

    Add assign-notifier implement

---
+++ github/notion/assign-notifier/src/assign-notifier.rs
@@ -0,0 +1,31 @@
+#[allow(unused_imports)]
+use wasmedge_bindgen::*;
+use wasmedge_bindgen_macro::*;
+use serde_json::Value;
+
+#[wasmedge_bindgen]
+pub fn run(s: String) -> Result<String, String> {
+    let json = match serde_json::from_str::<Value>(s.as_str()) {
+        Ok(s) => s,
+        Err(e) => {
+            return Err(format!(
+                "GitHub webhook payloads parsing failed: {}.",
+                e.to_string()
+            ))
+        }
+    };
+
+    if let Some(action) = json.get("action") {
+        if action != "assigned" {
+            return Ok("".to_string());
+        }
+    } else {
+        return Err("Failed to get action".to_string());
+    }
+
+    if let Some(url) = json["issue"]["html_url"].as_str() {
+        Ok(url.to_string())
+    } else {
+        Err("Failed to get issue url".to_string())
+    }
+}
