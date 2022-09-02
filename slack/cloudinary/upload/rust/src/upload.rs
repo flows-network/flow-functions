0edea10b57f6bc097283c220a805df9e89edeb2a
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-09-02 11:39:17 +0800

    Update Cloudinary upload function

--- slack/cloudinary/upload/rust/src/upload.rs
+++ slack/cloudinary/upload/rust/src/upload.rs
@@ -1,7 +1,6 @@
 #[allow(unused_imports)]
 use wasmedge_bindgen::*;
 use wasmedge_bindgen_macro::*;
-use serde_json::json;
 use regex::Regex;
 
 #[wasmedge_bindgen]
@@ -10,17 +9,17 @@ pub fn run_file_1(
 ) -> (String, String, String, Vec<u8>) {
     return (
         "".to_string(),     // (omit)
-        name,               // File name without extension (required)
-        mimetype,           // File MIME type (required)
-        file,               // File data
+        name,               // File name (optional)
+        "".to_string(),     // (omit)
+        file,               // File data (required)
     );
 }
 
 #[wasmedge_bindgen]
-pub fn run(s: String) -> String {
-    let reg = Regex::new("<(\\S+)>").unwrap(); // Because slack will add angle brackets around the link
-    return json!({
-        "url": reg.captures(&s).unwrap().get(1).unwrap().as_str(),
-        "file_type": "image"    // It must be image/video/raw, 
-    }).to_string();
+pub fn run_file_url(s: String) -> String {
+    // Because slack will add angle brackets around the link
+    let reg = Regex::new("<(\\S+)>").unwrap(); 
+
+    // Return the file URL to Cloudinary
+    return reg.captures(&s).unwrap().get(1).unwrap().as_str().to_string();
 }
