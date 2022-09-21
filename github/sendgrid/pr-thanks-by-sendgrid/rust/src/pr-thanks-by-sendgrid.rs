92a23c80518d60117cbc2b36e710a30812fdbd8d
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-09-21 09:20:57 +0800

    Update github/sendgrid/pr-thanks-by-sendgrid

--- github/sendgrid/pr-thanks-by-sendgrid/rust/src/pr-thanks-by-sendgrid.rs
+++ github/sendgrid/pr-thanks-by-sendgrid/rust/src/pr-thanks-by-sendgrid.rs
@@ -1,6 +1,5 @@
-use serde_json::json;
-use flows_connector_dsi::{github::inbound};
 use wasmedge_bindgen_macro::*;
+use flows_connector_dsi::{github::inbound, sendgrid::outbound};
 
 #[wasmedge_bindgen]
 pub fn run(s: String) -> Result<String, String> {
@@ -11,40 +10,28 @@ pub fn run(s: String) -> Result<String, String> {
 
 fn _run(s: String) -> Result<String, String> {
     let payload = inbound(s)?;
-    
-    payload.get_pull_request()?;
+    let email = match payload
+        .pull_request
+        .as_ref()
+        .and(payload.sender.email.as_ref())
+    {
+        Some(e) => e,
+        None => return Ok(String::new()),
+    };
 
     if payload.get_action()? != "opened" {
         return Ok(String::new());
     }
-    
-    let subject = "Thank you for contributing to this repository";
-    let content = format!(
+
+    outbound(vec![email])
+        .subject("Thank you for contributing to this repository")
+        .content(format!(
             r#"
 Hi {}, <br/>
 
-Welcome to the {} community, thank you for your contribution!
-                        "#,
-            payload.sender.login, payload.repository.unwrap().full_name
-        );
-    
-
-    Ok(json!([{
-                            "personalizations": [
-                                {
-                                    "to": [
-                                        {
-                                            "email": payload.sender.email,
-                                        }
-                                    ]
-                                }
-                            ],
-                            "subject": subject,
-                            "content": [
-                                {
-                                    "type": "text/html",
-                                    "value": content
-                                }
-                            ]
-                        }]).to_string())
+Welcome to the {} community, thank you for your contribution!"#,
+            payload.sender.login,
+            payload.repository.unwrap().full_name
+        ))
+        .build()
 }
