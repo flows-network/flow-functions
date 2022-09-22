9e8145725e27e0ca65b192b7ae439f1c24bfb802
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-09-22 09:31:46 +0800

    Fix issue-creator

--- slack/github/issue-creator/rust/src/issue-creator.rs
+++ slack/github/issue-creator/rust/src/issue-creator.rs
@@ -1,8 +1,11 @@
 #[allow(unused_imports)]
 use wasmedge_bindgen::*;
 use wasmedge_bindgen_macro::*;
+use flows_connector_dsi::github::outbound;
 
 #[wasmedge_bindgen]
-pub fn run(s: String) -> String {
-    s
+pub fn run(s: String) -> Result<String, String> {
+    outbound::create_issue(&s)
+        .body(s)
+        .build()
 }
