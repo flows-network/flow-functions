cf7d23d0c7756bbf1475c3fd0ce522d2c82096d3
Ho 229 <ho229v3666@gmail.com>
2022-09-30 23:12:52 +0800

    Jira dev (#38)
    * Add jira-pr-status-updater
    * Add sync-pr-to-jira

---
+++ github/jira/sync-pr-to-jira/rust/src/sync-pr-to-jira.rs
@@ -0,0 +1,28 @@
+use flows_connector_dsi::{github::inbound, jira::outbound};
+use wasmedge_bindgen_macro::*;
+
+#[wasmedge_bindgen]
+pub fn run(s: String) -> Result<String, String> {
+    #[allow(unused_imports)]
+    use wasmedge_bindgen::*;
+    _run(s)
+}
+
+pub fn _run(s: String) -> Result<String, String> {
+    let payload = inbound(s)?;
+
+    if payload.get_action()? != "opened" {
+        return Ok(String::new());
+    }
+
+    let pull_request = payload.get_pull_request()?;
+
+    outbound::create_issue(&pull_request.title)
+        .description(
+            pull_request
+                .body
+                .as_ref()
+                .unwrap_or(&"No description.".to_string()),
+        )
+        .build()
+}
