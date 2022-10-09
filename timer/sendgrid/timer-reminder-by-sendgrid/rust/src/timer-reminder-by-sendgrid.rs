a583688545529951f1428b9bc051e3be7560cd16
jaykchen <jaykchen@gmail.com>
2022-10-09 14:29:24 -0400

    add timer-reminder-by-sendgrid.rs

---
+++ timer/sendgrid/timer-reminder-by-sendgrid/rust/src/timer-reminder-by-sendgrid.rs
@@ -0,0 +1,25 @@
+use flows_connector_dsi::{ sendgrid::outbound};
+use wasmedge_bindgen_macro::*;
+
+#[wasmedge_bindgen]
+pub fn run(s: String) -> Result<String, String> {
+    #[allow(unused_imports)]
+    use wasmedge_bindgen::*;
+    _run(s)
+}
+
+pub fn _run(s: String) -> Result<String, String> {
+    if s.trim().is_empty() {
+        return Err("Wrong trigger is emitted somewhere".to_string());
+
+    }
+    outbound(vec!["jaykchen@gmail.com", "achenics@gmail.com"])
+        .subject("This is reminder of your task on xxx")
+        .content(format!(
+            r#"
+Hi,<br/>
+
+Please be kindly reminded that the monthly meeting is scheduled at 10am on 11, Oct."#,
+        ))
+        .build()
+}
