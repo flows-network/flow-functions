df1fba50a7eca67258430a4d63fe8ac9bc4fa9a6
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-10-21 11:47:48 +0800

    Update index.js

--- discord/discord/welcome/javascript/index.js
+++ discord/discord/welcome/javascript/index.js
@@ -2,6 +2,6 @@
     let message = JSON.parse(s);
     if (message["type"] === 7)          // MemberJoin event
         return JSON.stringify({
-            content: "Welcome @" + message["content"],
+            content: "welcome <@" + message["author"]["id"] + ">!",
         })
 }
