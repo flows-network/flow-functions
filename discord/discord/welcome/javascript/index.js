212dfb887125cd3e408e402dcadf451f71492018
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-10-21 13:00:50 +0800

    Add rust version of welcome for discord

--- discord/discord/welcome/javascript/index.js
+++ discord/discord/welcome/javascript/index.js
@@ -2,6 +2,6 @@
     let message = JSON.parse(s);
     if (message["type"] === 7)          // MemberJoin event
         return JSON.stringify({
-            content: "welcome <@" + message["author"]["id"] + ">!",
+            content: "Welcome <@" + message["author"]["id"] + ">!",
         })
 }
