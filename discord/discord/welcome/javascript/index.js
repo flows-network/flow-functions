271196a7396303c8c7e764b6981bbc483fd30b2e
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-10-21 11:32:39 +0800

    Add js version of welcome for discord

---
+++ discord/discord/welcome/javascript/index.js
@@ -0,0 +1,7 @@
+(s) => {
+    let message = JSON.parse(s);
+    if (message["type"] === 7)          // MemberJoin event
+        return JSON.stringify({
+            content: "Welcome @" + message["content"],
+        })
+}
