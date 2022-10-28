03de457c2b742d33fca409dccd2309723dc60ccf
ho-229 <56023510+ho-229@users.noreply.github.com>
2022-10-28 17:00:00 +0800

    Update issue-review-notifier.rs

--- github/github/issue-review-notifier/rust/src/issue-review-notifier.rs
+++ github/github/issue-review-notifier/rust/src/issue-review-notifier.rs
@@ -12,30 +12,27 @@ fn _run(s: String) -> Result<String, String> {
     let payload = inbound(s)?;
     let issue = payload.get_issue()?;
 
-    let mut assignees = Vec::new();
-    let mut body = String::new();
-
-    if issue.title.contains("platforms") || issue.title.contains("ci") {
-        assignees.push("jaykchen");
-    } else if issue.title.contains("docs") {
-        assignees.push("alabulei1");
-    } else {
-        body = format!(
-            "This issue title is not compliant with our community rules. @{}, please
-        update it. Valid types: docs, ci, platforms.",
-            payload.sender.login
-        );
-    }
+    let assignees = match issue.title.as_str() {
+        t if t.starts_with("platforms") || issue.title.starts_with("ci") => {
+            vec!["jaykchen"]
+        }
+        t if t.starts_with("docs") => vec!["alabulei1"],
+        _ => vec![],
+    };
 
     outbound::modify_issue(issue.number)
-        .body(if body.is_empty() {
+        .body(if assignees.is_empty() {
+            format!(
+                "This issue title is not compliant with our community rules. @{}, please \
+update it. Valid types: docs, ci, platforms.",
+                payload.sender.login
+            )
+        } else {
             format!(
                 "Thank you @{} for submitting this issue! It has been assigned to @{}",
                 payload.sender.login,
                 assignees.join(", @"),
             )
-        } else {
-            body
         })
         .assignees(assignees)
         .build()
